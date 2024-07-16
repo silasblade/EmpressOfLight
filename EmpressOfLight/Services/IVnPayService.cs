7a901f5e13d3c79925e875f0c625c1e014d8b2e0
PDai03 <4701104057@student.hcmue.edu.vn>
2024-07-16 15:51:17 +0700

    VnPayUpdate

---
+++ EmpressOfLight/Services/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using EmpressOfLight.Models.ViewModels;
+
+namespace EmpressOfLight.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(HttpContext context, VnPayRequestModel model);
+        VnPaymentResponseModel PaymentExcecute(IQueryCollection collections);
+    }
+}
